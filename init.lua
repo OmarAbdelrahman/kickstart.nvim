bb312d9984948dedc5722f1d3ce1acfb33bd6a53
OmarAbdelrahman <omar.sebres@gmail.com>
2024-06-25 14:21:13 +0200

    enable mini.files and config mapping

--- init.lua
+++ init.lua
@@ -854,6 +854,9 @@ require('lazy').setup({
 
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
+    keys = {
+      { '<leader>\\', ':lua MiniFiles.open()<CR>', { desc = 'open minifiles' } },
+    },
     config = function()
       -- Better Around/Inside textobjects
       --
@@ -869,6 +872,7 @@ require('lazy').setup({
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
+      require('mini.files').setup()
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
