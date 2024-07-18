8e41d309a788acd2ea4b0cacca552e4678de3aca
OmarAbdelrahman <omar.sebres@gmail.com>
2024-07-18 11:04:28 +0200

    avoid duplicate format_on_save

--- init.lua
+++ init.lua
@@ -230,7 +230,7 @@ require('lazy').setup({
     'LazyVim/LazyVim',
     import = 'lazyvim.plugins',
     opts = {
-      colorscheme = 'catppuccin-frappe',
+      colorscheme = 'kanagawa',
     },
   },
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
@@ -686,16 +686,16 @@ require('lazy').setup({
     },
     opts = {
       notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        return {
-          timeout_ms = 500,
-          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-        }
-      end,
+      -- format_on_save = function(bufnr)
+      --   -- Disable "format_on_save lsp_fallback" for languages that don't
+      --   -- have a well standardized coding style. You can add additional
+      --   -- languages here or re-enable it for the disabled ones.
+      --   local disable_filetypes = { c = true, cpp = true }
+      --   return {
+      --     timeout_ms = 500,
+      --     lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+      --   }
+      -- end,
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
